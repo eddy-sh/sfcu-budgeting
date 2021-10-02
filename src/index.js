4f9387c4259bcbc691d8b170dae6d891d56fb01f
Eddy <eddysh@protonmail.com>
2021-10-02 19:32:34 +0000

    Get total debit and credit data

--- src/index.js
+++ src/index.js
@@ -3,42 +3,86 @@ const path = require('path');
 const csv = require('fast-csv');
 
 const accounts = (function(accountDataPath) {
-  const init = (accountDataPath) => {
-    return fs.createReadStream(path.resolve(accountDataPath))
-        .pipe(csv.parse({ignoreEmpty: true, headers: [
-          'accountNumber',
-          'postDate',
-          'check',
-          'description',
-          'debit',
-          'credit',
-          'status',
-          'balance',
-          'classification',
-        ]}))
-        .transform((data) => ({
-          accountNumber: data.accountNumber,
-          post: data.postDate,
-          check: data.check,
-          description: data.description,
-          debit: data.debit,
-          credt: data.credit,
-          status: data.status,
-          balance: data.balance,
-          classification: data.classification,
-        }))
-        .on('error', (error) => console.error(error))
-        .on('data', (row) => console.log(row))
-        .on('end', (rowCount) => console.log(`Parsed ${rowCount} rows`));
+  let data = [];
+
+  const _getData = async (accountDataPath) => {
+    const rows = [];
+
+    const myPromise = new Promise((resolve, reject) => {
+      fs.createReadStream(path.resolve(accountDataPath))
+          .pipe(csv.parse({ignoreEmpty: true, headers: [
+            'accountNumber',
+            'postDate',
+            'check',
+            'description',
+            'debit',
+            'credit',
+            'status',
+            'balance',
+            'classification',
+          ]}))
+          .transform((data) => ({
+            accountNumber: data.accountNumber,
+            post: data.postDate,
+            check: data.check,
+            description: data.description,
+            debit: data.debit,
+            credt: data.credit,
+            status: data.status,
+            balance: data.balance,
+            classification: data.classification,
+          }))
+          .on('error', (error) => reject(error))
+          .on('data', (row) => rows.push(row))
+          .on('end', () => {
+            return resolve(rows);
+          });
+    });
+
+    await myPromise.then((res) => {
+      data = res;
+    }, (error) => {
+      console.log(error);
+    });
   };
 
-  const data = init(accountDataPath);
-
   return {
-    getAccounts: () => {
+    init: async () => {
+      await _getData(accountDataPath);
+    },
+
+    getData: () => {
       return data;
+    },
+
+    getTotalDebit: () => {
+      let amount = 0.0;
+      data.forEach((tx) => {
+        if (parseFloat(tx.debit)) {
+          amount += parseFloat(tx.debit);
+        }
+      });
+      return amount;
+    },
+
+    getTotalCredit: () => {
+      let amount = 0.0;
+      data.forEach((tx) => {
+        if (parseFloat(tx.credit)) {
+          amount += parseFloat(tx.credit);
+        }
+      });
+      return amount;
     },
   };
 });
 
-console.log(accounts(`./accounts/AccountHistory.csv`).getAccounts());
+const main = async () => {
+  const account = accounts('accounts/AccountHistory.csv');
+  await account.init();
+
+  console.log(`Debit: ${account.getTotalDebit()}`);
+  console.log(`Credit: ${account.getTotalCredit()}`);
+};
+
+main();
