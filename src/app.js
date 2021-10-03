1b28184b59e233a6175bf87155eb627ffcb2b1af
Eddy <eddysh@protonmail.com>
2021-10-03 15:58:22 +0000

    Add example endpoints

--- src/app.js
+++ src/app.js
@@ -11,6 +11,18 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('/data', (req, res) => {
+  res.send(`${account.getData()}`);
+})
+
+app.get('/queries/totalCredit', (req, res) => {
+  res.send(`${account.getTotalCredit()}`);
+})
+
+app.get('/queries/totalDebit', (req, res) => {
+  res.send(`${account.getTotalDebit()}`);
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
