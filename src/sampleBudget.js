af5fce40ead8ba9b08f5085b53c26cec40bbcbd7
Eddy <eddysh@protonmail.com>
2021-10-03 00:18:43 +0000

    Add sample budget to show data structure

---
+++ src/sampleBudget.js
@@ -0,0 +1,28 @@
+export const budgetGroups = {
+    Credit: [
+        'Wise money',
+        'ACH Credi Stanford'
+    ],
+    Donations: [],
+    Savings: [],
+    Rent: [],
+    Internet: [],
+    Utilities: [],
+    Household: [
+      'Starbucks',
+      'Philz',
+      'Grubhub',
+      'bytes cafe',
+      'amazon',
+      'atm withdrawal',
+      'uber'
+    ],
+    Activities: [],
+    OneOffs: [],
+    Subscriptions: [],
+    HealthInsurance: [],
+    PetInsurance: [],
+    EarthquakeInsurance: [],
+    RentInsurance: [],
+  };
+  
