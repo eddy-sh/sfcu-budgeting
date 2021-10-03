8bc888f4f2688f760c9e068319e82f745d47fce2
Eddy <eddysh@protonmail.com>
2021-10-03 15:38:14 +0000

    Add python/psql for SophieSchau

--- src/sampleBudget.js
+++ src/sampleBudget.js
@@ -1,28 +1,28 @@
 export const budgetGroups = {
-    Credit: [
-        'Wise money',
-        'ACH Credi Stanford'
-    ],
-    Donations: [],
-    Savings: [],
-    Rent: [],
-    Internet: [],
-    Utilities: [],
-    Household: [
-      'Starbucks',
-      'Philz',
-      'Grubhub',
-      'bytes cafe',
-      'amazon',
-      'atm withdrawal',
-      'uber'
-    ],
-    Activities: [],
-    OneOffs: [],
-    Subscriptions: [],
-    HealthInsurance: [],
-    PetInsurance: [],
-    EarthquakeInsurance: [],
-    RentInsurance: [],
-  };
+  Credit: [
+      'Wise money',
+      'ACH Credi Stanford'
+  ],
+  Donations: [],
+  Savings: [],
+  Rent: [],
+  Internet: [],
+  Utilities: [],
+  Household: [
+    'Starbucks',
+    'Philz',
+    'Grubhub',
+    'bytes cafe',
+    'amazon',
+    'atm withdrawal',
+    'uber'
+  ],
+  Activities: [],
+  OneOffs: [],
+  Subscriptions: [],
+  HealthInsurance: [],
+  PetInsurance: [],
+  EarthquakeInsurance: [],
+  RentInsurance: [],
+};
   
